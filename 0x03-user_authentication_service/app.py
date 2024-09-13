2e1446bf8ce63b7107128e7094190ba726e6f3da
kamau95 <linuskamau586@gmail.com>
2024-09-13 20:41:26 +0300

    auth process

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,10 @@
 """
 the factory for my flask app
 """
-from flask import Flask, jsonify, request
+from flask import redirect, url_for, make_response
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -35,6 +37,59 @@ def users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    auth = Auth()
+    if auth.valid_login(email, password):
+        session_id = auth.create_session(email)
+        response = make_response(
+            jsonify({'email': email, 'message': 'logged in'})
+        )
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    Logout by deleting the session.
+    Retrieves the session ID from
+    the request's cookies and destroys the session.
+    """
+    session_id = request.cookies.get('session_id')
+    if session_id:
+        auth = Auth()
+        user = auth.find_user_by(session_id=session_id)
+        if user:
+            auth.destroy_session(user.id)
+            return redirect(url_for('index'))
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """
+    Respond to GET profile request.
+    """
+    session_id = request.cookies.get('session_id')
+    auth = Auth()
+
+    try:
+        user = auth._db.find_user_by(session_id=session_id)
+    except NoResultFound:
+        abort(403)  # User not found, return a 403 Forbidden error
+
+    if user and session_id:
+        response = make_response(jsonify({"email": user.email}), 200)
+        return response
+
+    abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,10 +2,20 @@
 """
 thi si the auth module
 """
+from typing import Union, TypeVar
+import uuid
 import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+
+
+def _generate_uuid() -> str:
+    """
+    generates a uuid id and returns its str representation
+    """
+    id = str(uuid.uuid4())
+    return id
 
 
 def _hash_password(password: str) -> str:
@@ -35,6 +45,57 @@ class Auth:
             if existing_usr:
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            new_pass = _hash_password(password)
-            user = self._db.add_user(email, new_pass)
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        validates a user login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        if user and bcrypt.checkpw(
+            password.encode('utf-8'), user.hashed_password
+        ):
+            return True
+        return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create a session_id for an existing user and update the user's
+        session_id attribute
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        if user:
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """
+        retrievs a user by using session_id
+        """
+        if session_id is None:
+            return None
+        user = self._db.find_user_by(session_id=session_id)
+        if user is not None:
+            return user
+        return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroys a user's session by setting session_id to None.
+        """
+        user = self._db.find_user_by(user_id=user_id)
+        if user is None:
+            return
+        user.session_id = None
+        # save change to database
+        self._db.commit()
